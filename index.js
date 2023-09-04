76730e1b1bcc07a5d6c53e2f8a1f7fd27091576a
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-09-04 14:22:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -158,19 +158,18 @@ const fetchTechnologyNews = async () => {
 }
 
 const fetchQueryNews = async () => {
+    if (newsQuery.value == null) return;
 
-    if(newsQuery.value == null)
-        return;
+   const response = await fetch(SEARCH_NEWS + encodeURIComponent(newsQuery.value) + "&apiKey=" + API_KEY);
 
-    const response = await fetch(SEARCH_NEWS+encodeURIComponent(newsQuery.value)+"&apiKey="+API_KEY);
     newsDataArr = [];
-    if(response.status >= 200 && response.status < 300) {
+    if (response.status >= 200 && response.status < 300) {
         const myJson = await response.json();
         newsDataArr = myJson.articles;
     } else {
-        //error handle
+        // error handle
         console.log(response.status, response.statusText);
-        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        newsdetails.innerHTML = "<h5>No data found.</h5>";
         return;
     }
 
@@ -178,60 +177,41 @@ const fetchQueryNews = async () => {
 }
 
 function displayNews() {
-
     newsdetails.innerHTML = "";
 
-    // if(newsDataArr.length == 0) {
-    //     newsdetails.innerHTML = "<h5>No data found.</h5>"
-    //     return;
-    // }
-
     newsDataArr.forEach(news => {
-
         var date = news.publishedAt.split("T");
-        
         var col = document.createElement('div');
-        col.className="col-sm-12 col-md-4 col-lg-3 p-2 card";
-
+        col.className = "col-sm-12 col-md-4 col-lg-3 p-2 card";
         var card = document.createElement('div');
         card.className = "p-2";
-
         var image = document.createElement('img');
-        image.setAttribute("height","matchparent");
-        image.setAttribute("width","100%");
-        image.src=news.urlToImage;
-
+        image.setAttribute("height", "100%"); // Changed to "100%"
+        image.setAttribute("width", "100%");
+        image.src = news.urlToImage;
         var cardBody = document.createElement('div');
-        
         var newsHeading = document.createElement('h5');
         newsHeading.className = "card-title";
         newsHeading.innerHTML = news.title;
-
         var dateHeading = document.createElement('h6');
         dateHeading.className = "text-primary";
         dateHeading.innerHTML = date[0];
-
         var discription = document.createElement('p');
-        discription.className="text-muted";
+        discription.className = "text-muted";
         discription.innerHTML = news.description;
-
         var link = document.createElement('a');
-        link.className="btn btn-dark";
+        link.className = "btn btn-dark";
         link.setAttribute("target", "_blank");
         link.href = news.url;
-        link.innerHTML="Read more";
-
+        link.innerHTML = "Read more";
         cardBody.appendChild(newsHeading);
         cardBody.appendChild(dateHeading);
         cardBody.appendChild(discription);
         cardBody.appendChild(link);
-
         card.appendChild(image);
         card.appendChild(cardBody);
-
         col.appendChild(card);
-
         newsdetails.appendChild(col);
     });
-
 }
+
